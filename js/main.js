781d7853013e415fb03b2801161219f48006865a
Tomas Besso <tomi.besso12@gmail.com>
2023-09-22 21:06:38 -0300

    mejorar codigo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-// crear una funcion para calcular valor del m2
+// crear una funcion para calcular valor del riego
 
-// crear funcion para cuotas sin interes
+let valorMetroCuadrado = 500;
+let manoDeObra = 1.25;
+
+function calcularValorRiego () {
+
+    let continuar = true;
+    while (continuar) {
+        let largoTerreno = parseInt(prompt("Ingrese el largo del terreno en metros:"));
+        let anchoTerreno = parseInt(prompt("Ingrese el ancho del terreno en metros:"));
+
+        let areaTerreno = largoTerreno * anchoTerreno;
+
+        let valorRiego = (areaTerreno + valorMetroCuadrado) * manoDeObra;
+        alert("El valor del riego es de " + valorRiego + " Ars. Este es un valor aproximado del total del trabajo incluyendo insumos y mano de obra.");
+
+        let respuesta = prompt("Desea cotizar el valor de otro riego? Indique 'S' o 'N'.");
+
+        if (respuesta !== "S") {
+            continuar = false;  
+        }
+    }
+}
+
+calcularValorRiego();
+
+
+// crear funcion para cuotas
